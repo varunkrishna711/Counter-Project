872f9c3799f37160fb1043ac267e8d5565e594da
Varun Krishna <77630686+varunkrishna711@users.noreply.github.com>
2021-08-18 19:53:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Increment(){
+    let n=document.getElementById('num').innerHTML;
+    document.getElementById('num').innerHTML=Number(n)+1;
+}
+
+function Decrement(){
+    let n=document.getElementById('num').innerHTML;
+    document.getElementById('num').innerHTML=Number(n)-1;
+}
